380abc839beee685c4546e007f395019930ece35
wuzhoutiao <1822939315@qq.com>
2026-09-06 18:36:46 +0800

    Update ZooKeeper client connection to multiple hosts

--- netty-consumer/src/main/java/client/zk/ZooKeeperFactory.java
+++ netty-consumer/src/main/java/client/zk/ZooKeeperFactory.java
@@ -11,7 +11,7 @@ public class ZooKeeperFactory {
     public static CuratorFramework getClient(){
         if(client==null){
             RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3); //自动重试机制：初始等待1000ms，最多重试三次，并且每次失败等待时间会指数增长
-            client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy); //使用刚才定义的重试机制，创建一个 ZooKeeper 客户端，并连接到这台 ZooKeeper 服务(127.0.0.1:2181只是表示Zookeeper服务运行在哪儿，不是节点路径)
+            client = CuratorFrameworkFactory.newClient("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", retryPolicy); //使用刚才定义的重试机制，创建一个 ZooKeeper 客户端，并连接到这台 ZooKeeper 服务(127.0.0.1:2181只是表示Zookeeper服务运行在哪儿，不是节点路径)
             client.start();
         }
         return client; //真正启动客户端
